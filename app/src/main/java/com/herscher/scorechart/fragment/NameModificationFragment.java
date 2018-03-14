1ef13f6fe7f1b1570770caf3f4feff1a4ca17111
Mark Herscher <mherscher@ackmanndickenson.com>
2018-03-14 13:24:47 -0500

    Showing keyboard on dialog fragment

--- app/src/main/java/com/herscher/scorechart/fragment/NameModificationFragment.java
+++ app/src/main/java/com/herscher/scorechart/fragment/NameModificationFragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -76,6 +77,13 @@ public class NameModificationFragment extends DialogFragment {
             throw new IllegalStateException(
                     "Activity or Fragment must extend NameModificationFragment.Listener interface");
         }
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        // Show the keyboard (for some reason it doesn't want to by default)
+        super.onActivityCreated(savedInstanceState);
+        getDialog().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
     }
 
     @Override
